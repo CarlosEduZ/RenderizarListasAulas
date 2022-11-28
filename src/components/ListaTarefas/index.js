7e3fa9a5e3f7afaf280756f5627ef6b6fc080f44
CarlosEduZ <108636661+CarlosEduZ@users.noreply.github.com>
2022-11-28 20:30:42 -0300

    Initial commit

---
+++ src/components/ListaTarefas/index.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import {
+  InputContainer,
+  ListaContainer,
+  ListaTarefasContainer,
+  Tarefa,
+  TaskInput,
+  AddTaskButton,
+  RemoveButton
+} from "./styled";
+import bin from "../../assets/bin.png";
+
+export function ListaTarefas() {
+  const [novaTarefa, setNovaTarefa] = useState("");
+  const [lista, setLista] = useState(["beber água", "tomar banho", "assistir a copa"]);
+
+  const onChangeTarefa = (event) => {
+    setNovaTarefa(event.target.value);
+  };
+
+  const adicionaTarefa = () => {
+    const novaLista = [...lista, novaTarefa]
+    setLista(novaLista)
+    setNovaTarefa("")
+
+  };
+
+  const removeTarefa = (item) => {
+    const removeItem = lista.filter((tarefa) => tarefa !== item)
+    setLista(removeItem)
+  };
+
+  const listaTela = lista.map((item, index) => {
+    return(
+      <ul>
+          <Tarefa key={index}>
+            {item}
+            <RemoveButton onClick={()=>removeTarefa(item)}>
+              <img src={bin} alt="" width="16px" />
+            </RemoveButton>
+          </Tarefa>
+        </ul>
+    )
+  }) 
+
+  return (
+    <ListaTarefasContainer>
+      <InputContainer>
+        <TaskInput
+          placeholder="Digite aqui uma tarefa"
+          value={novaTarefa}
+          onChange={onChangeTarefa}
+        />
+        <AddTaskButton onClick={adicionaTarefa}>Adicionar</AddTaskButton>
+      </InputContainer>
+
+      <ListaContainer>
+        {listaTela}
+      </ListaContainer>
+    </ListaTarefasContainer>
+  );
+}
